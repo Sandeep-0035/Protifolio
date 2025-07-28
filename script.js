c4d727b9f740c9dbccb829286e284f5b00746f72
Sandeep-0035 <g.sandeepyadav85@gmail.com>
2025-07-28 15:01:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("Hello");
+
+x=null;
+console.log(x);
+console.log(typeof X);
+
+r=BigInt("123456");
+console.log(typeof r)
+
+const student = {
+    name: "sandeep",
+    age: 20,
+    skilled: true
+};
+console.log(student)
+let age=20
+if(age>=18){
+    console.log("eligible to vote");
+}
+else{
+    console.log("not eligble for vote");
+}
+
+for(let i=0;i<=10;i++){
+    console.log(i,"th iteratior");
+}
+
+function f1(a1,a2){
+    console.log(a1+a2);
+}
+f1(1,2);
+const c3=(p1,p2)=>{
+    return p1+p2;
+}
+
+s=c3(1,4);
+console.log(s);
+
